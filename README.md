095818a11d3a767ebc7dce6e2c1ca1de554aa1f5
Justin O'Connor <jsoconno@gmail.com>
2022-08-03 12:25:56 -0400

    add latest updates

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Terraform Environment Setup Using Azure CLI
 The `terraform-setup.sh` script in this repository can be used to stand up baseline resource required for Terraform.  It includes:
 
-* A resource group that contains all of the automation infrastructure.
-* A storage account and containers for Terraform state and plan management.
-* A key vault for managing secrets used as part of Terraform execution in pipelines.
+* A resource group that contains all of the automation infrastructure
+* A storage account and containers for Terraform state and plan management
+* A key vault for managing secrets used as part of Terraform execution in pipelines (optional)
 
 # How to use
 1. Navigate to this folder on the command line using `cd` commands.
@@ -16,46 +16,9 @@ Note: Secrets should never be added directly to a Git repository.  All output se
 
 For example, it is a good idea to remove the `access_key` value from the `terraform-backend` file and pass it to Terraform on the command line with the `-backend-config` flag or using environment variables.
 
-# Test Running Terraform
-Now that the baseline infrastructure is established, you can start to use the created service principal for running Terraform in the selected subscription(s).
+# Test run Terraform
+Now that the baseline infrastructure is established, you can start to use the created service principal for running Terraform in the selected subscription(s).  A script was automatically generated as part of running `./terraform-setup.sh` that makes this a simple process.
 
 1. Install Terraform.  A great tool to use is `tfenv`.  Here are the docs: https://github.com/tfutils/tfenv.
-2. Log in with the service principal using the details in the `credentials` file by running `az login --service-principal -u <app-id> -p <password-or-cert> --tenant <tenant>`
-3. Create a new Terraform project with the following script:
-
-```
-terraform {
-    backend "azurerm" {
-        resource_group_name  = "terraform-29953-dev"
-        storage_account_name = "terraform29953dev"
-        container_name       = "state"
-        key                  = "dev.terraform.tfstate"
-    }
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 3.0"
-        }
-    }
-}
-
-provider "azurerm" {
-    subscription_id = "00000000-0000-0000-0000-000000000000"
-    client_id       = "00000000-0000-0000-0000-000000000000"
-    client_secret   = ""
-    tenant_id       = "00000000-0000-0000-0000-000000000000"
-    features {
-    }
-}
-
-resource "azurerm_resource_group" "rides" {
-    name = "test-resource-group"
-    location = "eastus"
-}
-
-
-```
-   
-4. Run `terraform init -backend-config="access_key=YOUR_ACCESS_KEY"`
-5. Run `terraform plan -var="client_secret=YOUR_CLIENT_SECRET" -out out.tfplan`
-6. Run `terraform apply out.tfplan`
+2. Run `chmod +x ./auto-script.sh` to ensure you can run the script from the command line.
+3. Run `./auto-script.sh` on the command line to execute the script.
--- terraform-setup.sh
+++ terraform-setup.sh
@@ -30,7 +30,25 @@ ACCESS_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME 
 az storage container create --name $STATE_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --auth-mode login
 az storage container create --name $PLAN_CONTAINER_NAME --account-name $STORAGE_ACCOUNT_NAME --auth-mode login
 
-# 5. Get Terraform backend configuration
+# 5. Create the service principal for the target environment and get IDs
+export MSYS_NO_PATHCONV=1
+SP_CREDENTIALS=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME)
+echo "$SP_CREDENTIALS" > "credentials-$ENVIRONMENT.json"
+SPN_ID=$(az ad sp list --all --query "[?displayName=='$SERVICE_PRINCIPAL_NAME'].id" -o tsv)
+APP_ID=$(az ad app list --all --query "[?displayName=='$SERVICE_PRINCIPAL_NAME'].id" -o tsv)
+CLIENT_SECRET=$(grep -o '"password": "[^"]*' credentials-$ENVIRONMENT.json | grep -o '[^"]*$')
+
+# 6. Assign service principal roles and permissions
+az role assignment create --assignee $SPN_ID --role "Owner" --scope /subscriptions/$SUBSCRIPTION_ID
+az role assignment create --assignee $SPN_ID --role "Contributor" --scope /subscriptions/$SUBSCRIPTION_ID
+
+# 7. Create the key vault and add an access policy for Terraform
+az keyvault create --resource-group $RESOURCE_GROUP_NAME --location $RESOURCE_GROUP_LOCATION --name $KEY_VAULT_NAME
+az keyvault set-policy --name $KEY_VAULT_NAME --secret-permissions all --key-permissions all --certificate-permissions all --object-id $APP_ID
+
+# 8. Create a working Terraform test script to validate credentials
+TENANT_ID=$(az account show --query tenantId -o tsv)
+CLIENT_ID=$(az ad app list --all --query "[?displayName=='$SERVICE_PRINCIPAL_NAME'].appId" -o tsv)
 TERRAFORM_BACKEND=$(cat <<EOF
 terraform {
     backend "azurerm" {
@@ -40,22 +58,41 @@ terraform {
         key                  = "$ENVIRONMENT.terraform.tfstate"
         access_key           = "$ACCESS_KEY"
     }
+    required_providers {
+        azurerm = {
+            source  = "hashicorp/azurerm"
+            version = "~> 3.0"
+        }
+    }
+}
+
+provider "azurerm" {
+    subscription_id = "$SUBSCRIPTION_ID"
+    client_id       = "$CLIENT_ID"
+    client_secret   = "$CLIENT_SECRET"
+    tenant_id       = "$TENANT_ID"
+    features {
+    }
+}
+
+resource "azurerm_resource_group" "example" {
+    name = "test-resource-group"
+    location = "eastus"
 }
 EOF
 )
 echo "$TERRAFORM_BACKEND" > "terraform-backend-$ENVIRONMENT.tf"
 
-# 6. Create the service principal for the target environment and get IDs
-export MSYS_NO_PATHCONV=1
-SP_CREDENTIALS=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --role Contributor --scopes /subscriptions/$SUBSCRIPTION_ID)
-echo "$SP_CREDENTIALS" > "credentials-$ENVIRONMENT.json"
-SPN_ID=$(az ad sp list --all --query "[?displayName=='$SERVICE_PRINCIPAL_NAME'].appId" -o tsv)
-APP_ID=$(az ad app list --all --query "[?displayName=='$SERVICE_PRINCIPAL_NAME'].appId" -o tsv)
-OBJECT_ID=$(az ad app list --all --query "[?displayName=='$SERVICE_PRINCIPAL_NAME'].id" -o tsv)
-
-# 7. Create the key vault and add an access policy for Terraform
-az keyvault create --resource-group $RESOURCE_GROUP_NAME --location $RESOURCE_GROUP_LOCATION --name $KEY_VAULT_NAME
-az keyvault set-policy --name $KEY_VAULT_NAME --secret-permissions all --key-permissions all --certificate-permissions all --object-id $OBJECT_ID
+# 9. Create a file to make running Terraform code simpler
+TERRAFORM_RUN=$(cat <<EOF
+az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET --tenant $TENANT_ID
+terraform init
+terraform plan -out out.tfplan
+terraform apply out.tfplan
+terraform apply -destroy -auto-approve
+EOF
+)
+echo "$TERRAFORM_RUN" > "terraform-run.sh"
 
 # X. Clean up (optional)
 if [ $CLEAN_UP = true ]
